334896fcfb2944ee823348f4c23d526975ef01a2
saitanuja08 <138033514+saitanuja08@users.noreply.github.com>
2023-10-02 07:40:47 -0400

    Update backend.tf

--- nonprod/azure-infra/backend.tf
+++ nonprod/azure-infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "onboarding-rg"
-    storage_account_name = "onboardingtfstatefiles"
+    storage_account_name = "onbterraformstate"
     container_name       = "tfstate"
     key                  = "nonprod.terraform.tfstate"
   }
